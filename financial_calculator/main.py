160545eeed3b37caf623b8b5911c5168ccf151ed
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-01-08 13:44:26 -0700

    Start

---
+++ financial_calculator/main.py
@@ -0,0 +1,21 @@
+#Asher Wangia Financial Calculator
+#this is the financfial calculator program
+
+def tip_calc():
+    pass
+
+def sale_price_calc():
+    pass
+
+def compound_intrest_calc():
+    initial_amount = input("How much money is in your account at the start: ")
+    intrest = input("How much is your intrest in percent: ")
+
+def budget_allocator():
+    pass
+
+def save_calc():
+    pass
+
+def main():
+    pass
