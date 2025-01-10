2d0dbf66401154e1a89ea0fd95a4fdeaad13fcfc
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-01-10 13:19:17 -0700

    sdf

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -1,5 +1,8 @@
 #Asher Wangia Financial Calculator
 #this is the financfial calculator program
+intrest = 0
+time = 0
+money_amount = 0
 
 def tip_calc():
     pass
@@ -7,15 +10,43 @@ def tip_calc():
 def sale_price_calc():
     pass
 
-def compound_intrest_calc():
-    initial_amount = input("How much money is in your account at the start: ")
-    intrest = input("How much is your intrest in percent: ")
-
+def compound_intrest_calc(money_amount,time):
+    while time > 0:
+        money_amount += money_amount*intrest
+        time -= 1
+        return money_amount
 def budget_allocator():
     pass
 
 def save_calc():
     pass
 
-def main():
+def main(money_amount,intrest,time):
+    print("""
+    1. Save Goal Calculator
+    2. Compound Intrest Calculator
+    3. Budget Allocator
+    4. Sale Price Calculator
+    5. Tip Calculator
+    """)
+    tool = input("Choose One Number: ")
+    
+    if tool == "1":
+        save_calc()
+    elif tool == "2":
+        money_amount = int(input("How much money is in your account at the start: "))
+        intrest = (float(input("How much is your intrest in percent: ")))/100
+        time = int(input("How many years: "))
+        compound_intrest_calc(money_amount,time)
+        print("Here is your total amount of money after intrest",money_amount)
+
+    elif tool == "3":
+        budget_allocator()
+    elif tool == "4":
+        sale_price_calc()
+    elif tool == "5":
+        tip_calc()
     pass
+
+main(money_amount,intrest,time)
+    
--- financial_calculator/test.py
+++ financial_calculator/test.py
@@ -1,18 +1,16 @@
-money_amount = 0
-intrest = 0
-time = 0
 
 
 
-money_amount = int(input("Money: "))
-intrest = (int(input("Intrest: ")))/100
-time = int(input("Time: "))
+
 
 def calc(money_amount):
-    money_amount = money_amount + money_amount*intrest
+    money_amount = int(input("Money: "))
+    intrest = (int(input("Intrest: ")))/100
+    time = int(input("Time: "))
+    while time > 0:
+        money_amount += money_amount*intrest
+        time -=1
     return money_amount
 
-calc(money_amount)*time
 
-print(money_amount)
 
