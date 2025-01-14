dc64b563a1677dcbf5d8742ca1ac68f335375667
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-01-14 13:44:12 -0700

    dfg

---
+++ personal_library/main.py
@@ -0,0 +1,35 @@
+#Asher Wangia, Personal Library Program
+
+storage = []
+
+running = True
+
+
+
+def main():
+    pass
+
+    print("""
+    This is a Library to store Books
+    1. Add Books
+    2. Remove Books
+    3. Search
+    4. Done
+    """)
+
+    choice = input("Choose a Number: ")
+
+    if choice == "1":
+        book = input("What is the book you want to add: ")
+        author = input("What is the author of the book you choose: ")
+        add()
+def add():
+    pass
+
+def remove():
+    pass
+
+while running == False:
+    main()
+
+main()
