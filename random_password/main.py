beae64a6f16eb601f48fc2b103efaf4454db8a55
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-01-28 07:02:49 +0000

    Done

--- random_password/main.py
+++ random_password/main.py
@@ -2,19 +2,24 @@
 import random
 import string
 
-upper_char = (string.ascii_uppercase).split()
-lower_char = (string.ascii_lowercase).split()
-num_char = (string.digits).split()
-special_char = (string.punctuation).split()
+upper_char = (string.ascii_uppercase)
+lower_char = (string.ascii_lowercase)
+num_char = (string.digits)
+special_char = (string.punctuation)
 
 characters = []
 
 
 password = []
+password2 = []
+password3 = []
+password4 = []
 
+
+
+#This function asks the user for the requirements
 def main():
     
-    chars = int(input("How Many Characters In Length: "))
 
     while True:
         print("""
@@ -36,19 +41,39 @@ def main():
         elif req == "4":
             special()
         else:
-            for x in range(chars): 
-                pass_generator()
+            pass_generator(int(input("How Many Characters In Length: ")))
 
-            print("Here is your password:", password)
-
+            
             exit()
 
 
 
-def pass_generator():
-    password.append(random.choice(characters)) 
-       
-
+#This Function generates the random characters that will go in the password and combines them
+def pass_generator(chars):
+    
+    characters_done = "".join(characters)
+    
+    for i in range(chars):
+        random_char = random.choice(characters_done)
+        password.append(random_char)
+    print("Here is your password:", "".join(password))
+        
+    for i in range(chars):
+        random_char = random.choice(characters_done)
+        password2.append(random_char)
+    print("Here is your password:", "".join(password2))
+        
+    for i in range(chars):
+        random_char = random.choice(characters_done)
+        password3.append(random_char)
+    print("Here is your password:", "".join(password3))
+    
+    for i in range(chars):
+        random_char = random.choice(characters_done)
+        password4.append(random_char)
+    print("Here is your password:", "".join(password4))
+        
+     
     
 
 def upper():
