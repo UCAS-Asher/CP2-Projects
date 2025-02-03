267e535b0827cf7751e6e2ecd105acd7a2afe219
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-02-03 13:44:06 -0700

    Done

--- morse_code/main.py
+++ morse_code/main.py
@@ -3,8 +3,9 @@ import string
 
 
 english = list(string.ascii_lowercase)
+english.append(" ")
 
-morse_code = ["._","_...","_._.","_..",".",".._.","__.","....","..",".___","_._","._..","__","_.","___",".__.","__._","._.","...","_",".._","..._",".__","_.._","_.__","__.."]
+morse_code = ["._","_...","_._.","_..",".",".._.","__.","....","..",".___","_._","._..","__","_.","___",".__.","__._","._.","...","_",".._","..._",".__","_.._","_.__","__..", " "]
 
 
 def main():
@@ -21,17 +22,21 @@ def main():
     elif choice == "2":
         morse_code_message = []
         
-        while True:
+        add_letters = True
+        while add_letters == True:
             print("""
+            Do you want to continue adding letters
             1. Yes
             2. No
             """)
-            morse_code_choice = input("Do you want to continue adding letters: ")
+            morse_code_choice = input("Choose a Number: ")
             if morse_code_choice == "1":
                 morse_code_letter = input("What is your letter in morse code: ")
                 morse_code_message.append(morse_code_letter)
             elif morse_code_choice == "2":
                 trans_morse_code(morse_code_message)
+                add_letters = False
+                
     else:
         exit()
     
