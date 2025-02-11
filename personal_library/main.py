e550dd83e4cd8b96d912d7c17aaf49dca13cefce
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-02-11 13:43:58 -0700

    asdfgh

--- personal_library/main.py
+++ personal_library/main.py
@@ -1,8 +1,6 @@
 #Asher Wangia, Personal Library Program
 
-books = set()
-authors = set()
-storage = {}
+library = []
 
 
 #This function runs other functions based on your choices
@@ -34,7 +32,18 @@ def main():
         """)
         search(input("Choose a Number: ").lower(),input("Which Book do you want to Search: ").lower())
     elif choice == "4":
-        print("Book Names:",books)
+        print("""
+        Choices
+        1. Simple Display
+        2. Detailed Display
+        """)
+        
+        display_type = input("Choose a Number: ")
+        
+        if display_type == "1":
+            display_simple()
+        elif display_type == "2":
+            display_complex()
     elif choice == "5":
         exit()
     
@@ -42,21 +51,29 @@ def main():
 #This function adds a book to the library along with all its other details
 def add_library(book, author, genre, year, pages):   
     
-    if book in books:
-        print("Book has Alredy Been Added")
-    else:
-        books.add(book)
-        authors.add(author)
-        storage.update({book: [author,genre,year,pages]})
+    title = input("What is the book you want to add: ").lower(), 
+    author = input("What is the author: ").lower(), 
+    genre = input("What is the genre: ").lower(), 
+    year = input("What year was the book released: ").lower(), 
+    pages = input("What is the amount of pages: ").lower()
+
+    book = {
+       "Title": book,
+       "Author": author,
+       "Genre": genre,
+       "Realese Date": year,
+       "Pages": pages
+   }
+
+    library.append(book)
+    print("Book Added")
+    
 
 
 #This function removes a book from the library
-def removelist(book):
-    if book in books:
-        books.remove(book)
-        del storage[book]
-    else:
-        print("Not In Library")
+def removelist(to_remove):
+    for book in library:
+        if to_remove in book["title"]
 
 
 #This function displays the details of a book based on which type of display is chosen
@@ -77,9 +94,19 @@ def search(search_type,book):
 
 
 
-
-
-
+def display_simple():
+    for book in storage:
+        print("Book:", book)
+        print("Author:",storage[book][0])
+        print(" ")
+def display_complex():
+    for book in storage:
+        print("Book:", book)
+        print("Author:",storage[book][0])
+        print("Genre:",storage[book][1])
+        print("Publish Date:",storage[book][2])
+        print("Pages:",storage[book][3])
+        print(" ")
 
 while True:
     main()
