a0e72137ea826665aab06961194e1775d82e2f32
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-02-14 18:38:18 -0700

    working on it

--- movie_recommeder/main.py
+++ movie_recommeder/main.py
@@ -3,7 +3,7 @@ import csv
 
 movie_storage = []
 
-with open("movie_recommeder/Movies list.csv") as file:
+with open("CP2-Projects/movie_recommeder/movie_list.csv") as file:
     movies = csv.reader(file)
     next(movies)
     for row in movies:
@@ -12,18 +12,64 @@ with open("movie_recommeder/Movies list.csv") as file:
        "Director": row[1],
        "Genre": row[2],
        "Rating": row[3],
-       "Length(Minutes)": row[4],
-       "Notable Actors": row[5]
+       "Length": row[4],
+       "Actors": row[5]
    }
         movie_storage.append(movie)
 
 
 def main():
+    
     print("""
     This is a Movie Recommender
-    1. Display
-    2. Remove Books
-    3. Search
-    4. Display Books(Author and Name or All Details)
-    5. Exit
+    1. Display All Movies
+    2. Get Recommendation
+    3. Filter Movies
+    4. Exit Program
     """)
+
+    choice = input("Choose a Number: ").lower()
+
+    if choice == "1":
+        display_movies()
+    elif choice == "2":
+        recommend_movie()
+    elif choice == "3":
+        pass
+    else:
+        exit()
+
+
+def display_movies():
+    for movie in movie_storage:
+        print("Movie:", movie["Title"])
+        print("Director:", movie["Director"])
+        print("Genre:", movie["Genre"])
+        print("Rating: ", movie["Rating"])
+        print("Length(Minutes):", movie["Length"])
+        print("Notable Actors:", movie["Actors"])
+        print(" ")
+        print(" ")
+
+
+def recommend_movie():
+    recommend_movies = []
+    
+    
+    genre = input("What genre do you like: ")
+    rating = input("What rating do you like: ")
+    
+    
+    with open("CP2-Projects/movie_recommeder/movie_list.csv") as file:
+        movies = csv.reader(file)
+        next(movies)
+        for movie in movie_storage:
+            for row in movies:
+                if genre in row and rating in row:
+                    recommend_movies.append(movie["Title"])
+                
+    print(recommend_movies)
+    
+
+while True:
+    main()
