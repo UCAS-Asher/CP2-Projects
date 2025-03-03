07ee2ecf8ffabe44205cde255538329ffce01608
UCAS-Asher <asher.wangia@ucas-edu.net>
2025-03-03 19:18:09 +0000

    sdfghj

--- word_counter/main.py
+++ word_counter/main.py
@@ -1,73 +1,69 @@
 #Asher Wangia, Word Counter
+import os
+
 
 from time_handling import time_set
 
-def count_words(user_file):
-    try:
-        with open(user_file, "r",) as file:
-            words = 0
-            file = file.read()
+def check_file(user_file):
+    exists = os.path.isfile(user_file)
 
-            rows = file.split()
-            words += len(rows)
-        if words > 1:  
-            return"There are " + str(words) + " words"
-        else:
-            return"There is " + str(words) + " word"
-    except:
+    if exists == True:
         pass
+    else:
+        print("Not a File Path... Try Again")
+        user_file = input("Input a relative path for a text file: ")
+        check_file(user_file)
+
+def count_words():
+    with open(user_file, "r",) as file:
+        words = 0
+        file = file.read()
+
+        rows = file.split()
+        words += len(rows)
+    if words > 1:  
+        return"There are " + str(words) + " words"
+    else:
+        return"There is " + str(words) + " word"
 
 
-def add_file(user_file):
-    try:
-        adding = input("What do you want to add to the file: ")
+
+def add_file():
+    adding = input("What do you want to add to the file: ")
             
-        with open(user_file, "a") as file:
-            file.write(adding + "\n")
-    except:
-        print("Not a Valid Relative Path\n")
+    with open(user_file, "a") as file:
+        file.write(adding + "\n")
 
 
-def remove_file(user_file):
-    try:
-        with open(user_file, "r+",) as file:
-            removing = input("What do you want to remove from the file: ")
+def remove_file():
+    with open(user_file, "r+",) as file:
+        removing = input("What do you want to remove from the file: ")
 
                 
-            for item in file:
-                if removing in item:
-                    with open(user_file, "r") as f:
-                        lines = f.readlines()
-                    with open(user_file, "w") as f:
-                        for line in lines:
-                            if line.strip("") != item:
-                                f.write(line)
+        for item in file:
+            if removing in item:
+                with open(user_file, "r") as f:
+                    lines = f.readlines()
+                with open(user_file, "w") as f:
+                    for line in lines:
+                        if line.strip("") != item:
+                            f.write(line)
                 
-                print("\nRemoved Item")
-    except:
-        print("Not a Valid Relative Path\n")
+            print("\nRemoved Item")
 
 
-def reset_file(user_file):
-    try:
-        open(user_file, "w").close()
-    except:
-        print("Not a Valid Relative Path\n")
+def reset_file():
+    open(user_file, "w").close()
 
 
-def display_file(user_file):
-    try:
-        with open(user_file, "r",) as file:
-            content = file.read()
-            print("File:")
-            print(content)
-    except:
-        print("Not a Valid Relative Path\n")
+def display_file():
+    with open(user_file, "r",) as file:
+        content = file.read()
+        print("File:")
+        print(content)
 
 
 def main():
-    
-    user_file = input("Input a relative path for a text file: ")
     
     print("""
     Choices
@@ -75,32 +71,39 @@ def main():
     2. Remove From File
     3. Reset File
     4. Display File
-    5. Exit
+    5. Use A Diffrent File
+    6. Exit
     """)
 
     choice = input("\nChoose a Number: ")
 
     if choice == "1":
-        add_file(user_file)
-        print("\n",count_words(user_file))
+        add_file()
+        print("\n",count_words())
         print(time_set())
     elif choice == "2":
-        remove_file(user_file)
-        print("\n",count_words(user_file))
+        remove_file()
+        print("\n",count_words())
         print(time_set())
     elif choice == "3":
-        reset_file(user_file)
-        print("\n",count_words(user_file))
+        reset_file()
+        print("\n",count_words())
         print(time_set())
     elif choice == "4":
-        display_file(user_file)
-        print("\n",count_words(user_file))
+        display_file()
+        print("\n",count_words())
         print(time_set())
     elif choice == "5":
+        user_file = input("Input a relative path for a text file: ")
+        check_file(user_file)
+    elif choice == "6":
         exit()
     else:
         print("Not an Option\n")
 
+
+user_file = input("Input a relative path for a text file: ")
+check_file(user_file)
 
 while True:
     main()
