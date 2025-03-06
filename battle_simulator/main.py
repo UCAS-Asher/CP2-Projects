47e41aa3efc318cf1e064677c7c821ff20ad078b
UCASAsher <asher.wangia@ucas-edu.net>
2025-03-06 13:43:27 -0700

    dfjm

---
+++ battle_simulator/main.py
@@ -0,0 +1,25 @@
+#Asher Wangia, Battle Simulator
+import csv
+
+def display_characters():
+    pass
+
+def create():
+    with open("battle_simulator/characters.csv") as file:
+        characters = csv.reader(file)
+
+
+def save():
+    pass
+
+
+def load():
+    pass
+
+
+def main():
+    pass
+
+
+while True:
+    main()
