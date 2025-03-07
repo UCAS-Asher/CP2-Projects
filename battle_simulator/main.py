9a8b305d82eace86405ff9c32e7d98807398f770
UCASAsher <asher.wangia@ucas-edu.net>
2025-03-07 13:44:03 -0700

    asdfjkl

--- battle_simulator/main.py
+++ battle_simulator/main.py
@@ -35,39 +35,61 @@ def create():
                 1. Assasin(Focusses on Speed)
                 2. Warrior(Foccuses on Strength)
                 3. Mage(Foccuses on Magic Power)
-                5. Tank(Foccuses on Defense)
-                6. No Class(Stats are Balanced)
+                4. Tank(Foccuses on Defense)
+                5. Jack of All Trades(Stats are Balanced)
                 """)
                 
                 char_class = input("Choose a Number for your Class")
                 
                 if char_class == "1":
                     health = 100
-                    strength = 20
-                    defense = 10
+                    strength = 25
+                    defense = 5
                     speed = 20
                     magic_power = 10
                     char_class = "Assasin"
                 elif char_class == "2":
-                    pass
+                    health = 100
+                    strength = 40
+                    defense = 10
+                    speed = 10
+                    magic_power = 1
+                    char_class = "Warrior"
                 elif char_class == "3":
-                    pass
+                    health = 100
+                    strength = 10
+                    defense = 1
+                    speed = 5
+                    magic_power = 45
+                    char_class = "Mage"
                 elif char_class == "4":
-                    pass
+                    health = 100
+                    strength = 25
+                    defense = 35
+                    speed = 1
+                    magic_power = 1
+                    char_class = "Tank"
                 elif char_class == "5":
-                    pass
-                elif char_class == "6":
-                    pass
+                    health = 100
+                    strength = 15
+                    defense = 15
+                    speed = 15
+                    magic_power = 15
+                    char_class = "Jack of All Trades"
                 else:
                     print("Not a Valid Class!")
 
             return char_class, health, strength, defense, speed, magic_power
 
-        character_values = get_class()
+    character_values = get_class()
 
-        char_class = character_values[0]
+    char_class = character_values[0]
+    health = character_values[1]
+    strength = character_values[2]
+    defense = character_values[3]
+    speed = character_values[4]
+    magic_power = character_values[5]
 
-        health = character_values[1]
         
     
     
